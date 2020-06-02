cdfb9d02e0447220e567f14299d666a0a9e50abb
chitesh <chiteshbansal111@gmail.com>
2020-06-02 15:50:17 +0530

    add increase functionality

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,6 +9,13 @@ class CartItem extends React.Component{
 			qty:1,
 			img:""
 		}
+
+		//this.increaseQty = this.increaseQty.bind(this);
+		// we can even bind the function when the constuctor is called
+		// another solution is that we can use arrow functions
+	}
+	increaseQty=()=>{
+		console.log(this.state);
 	}
 	render(){
 		const {price,title,qty}=this.state;
@@ -22,9 +29,22 @@ class CartItem extends React.Component{
 					<div style={{color:'#777'}}>Rs {price}</div>
 					<div style={{color:"#777"}}>Qty:{qty}</div>
 					<div className="cart-item-actions">
-					<img alt = "increase" className="action-icons" src="https://image.flaticon.com/icons/svg/864/864378.svg"/>
-					<img alt = "decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/864/864373.svg"/>
-					<img alt = "delete" className="action-icons" src="https://image.flaticon.com/icons/svg/2089/2089743.svg"/>
+					<img alt = "increase" 
+					className="action-icons"
+					 src="https://image.flaticon.com/icons/svg/864/864378.svg"
+					 onClick={this.increaseQty}//as this depends on
+					 // from which context hte funciton is called so 
+					 // when react internally called the function
+					 // then we get this as undefined so we need to bind the function 
+					 	/>
+					<img alt = "decrease"
+					 className="action-icons" 
+					 src="https://image.flaticon.com/icons/svg/864/864373.svg"
+					 />
+					<img alt = "delete"
+					 className="action-icons"
+					 src="https://image.flaticon.com/icons/svg/2089/2089743.svg"
+					 />
 					</div>
 				</div>
 			</div>
